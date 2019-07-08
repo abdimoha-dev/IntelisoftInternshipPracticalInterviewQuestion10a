b9dab1868389effe060222d5a6284017e64fa858
Etyang Emodo Japhlet <etyangemodojaphlet@yahoo.com>
2019-07-09 01:02:43 +0300

    One of the Object Oriented Programming principles is Inheritance. Demonstrate the following, and provide a test routine for your objects. a. How a mountain bicycle can inherit from a general class Bicycle. b. Patient can inherit from Person. c. A bird can inherit from an Animal class

---
+++ src/bicycle/MountainBicycle.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bicycle;
+
+/**
+ *
+ * @author Abdi Mohammed
+ */
+public class MountainBicycle extends Bicycle{
+    // the MountainBike subclass adds one more field 
+    public int seatHeight; 
+  
+    // the MountainBike subclass has one constructor 
+    public MountainBicycle(int gear,int speed, 
+                        int startHeight) 
+    { 
+        // invoking base-class(Bicycle) constructor 
+        super(gear, speed); 
+        seatHeight = startHeight; 
+    }  
+          
+    // the MountainBike subclass adds one more method 
+    public void setHeight(int newValue) 
+    { 
+        seatHeight = newValue; 
+    }  
+      
+    // overriding toString() method 
+    // of Bicycle to print more info 
+    @Override
+    public String toString() 
+    { 
+        return (super.toString()+ 
+                "\nseat height is "+seatHeight); 
+    } 
+    
+}
